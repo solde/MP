260f5cccf74336a6732bd20f9c1477de9fef3a9f
carachancla <43753630+carachancla@users.noreply.github.com>
2019-10-08 18:33:34 +0200

    H_dnw

--- PauDavid/LAB1/Segmentado/proyecto_1/mcd/control/CODIGO/control.vhd
+++ PauDavid/LAB1/Segmentado/proyecto_1/mcd/control/CODIGO/control.vhd
@@ -41,6 +41,11 @@ prx_esta: process(estado, op_dis, igualcero, consumo, menor, pcero)
 variable v_prxestado: tipoestado;
 begin
 
+	if estado = ESP and op_dis = '1' then v_prxestado := CALC;
+	elsif estado = CALC and igualcero = '1' then v_prxestado := HECHO;
+	elsif estado = HECHO and consumo = '1' then v_prxestado := ESP;
+	else v_prxestado := estado; 
+	end if;
 
 
 	prxestado <= v_prxestado after retardo_logica_estado;
@@ -51,8 +56,12 @@ logi_sal: process(estado, op_dis, igualcero, consumo, menor, pcero)
 variable v_ini, v_pe_a, v_pe_b: std_logic;
 variable v_finalizada, v_desocupada: std_logic;
 begin
-
-
+	
+	if estado = ESP and op_dis = '0' then v_desocupada := '1'; else v_desocupada := '0'; end if;
+	if estado = ESP and op_dis = '1' then v_ini := '1'; else v_ini := '0'; end if;
+	if estado = CALC and igualcero ='1' then v_finalizada := '1'; else v_finalizada := '0'; end if;
+	if igualcero='0' then v_pe_a := '1'; v_pe_b := '1'; else v_pe_a := '0'; v_pe_b := '0';end if;
+	
 ini <= v_ini after retardo_logica_salida;
 pe_a <= v_pe_a after retardo_logica_salida;
 pe_b <= v_pe_b after retardo_logica_salida;
