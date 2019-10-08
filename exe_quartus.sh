eaf95228397c363221ba1e76b31c9ac50bd8604e
Àlex Torregrosa <alextorre98@gmail.com>
2019-10-08 21:05:47 +0200

    Script execució quartus

---
+++ exe_quartus.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo docker run --rm -ti  -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v $(pwd):/home/usuari:rw -e DISPLAY=$DISPLAY --user=$(id -u):$(id -g) --ipc=host --cap-add=SYS_PTRACE --security-opt seccomp=unconfined registry.gitlab.com/axtaor/practicas-ac2:latest /root/runquartus.sh
