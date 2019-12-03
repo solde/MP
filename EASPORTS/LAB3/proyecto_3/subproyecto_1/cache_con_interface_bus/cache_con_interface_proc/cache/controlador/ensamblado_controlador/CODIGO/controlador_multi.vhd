5a5ea3a6f4574a7c1a779934bc105b6dc7e0ab2b
Àlex Torregrosa <alextorre98@gmail.com>
2019-12-03 12:16:07 +0100

    LAB3 Proyecto3_1

--- EASPORTS/LAB3/proyecto_3/subproyecto_1/cache_con_interface_bus/cache_con_interface_proc/cache/controlador/ensamblado_controlador/CODIGO/controlador_multi.vhd
+++ EASPORTS/LAB3/proyecto_3/subproyecto_1/cache_con_interface_bus/cache_con_interface_proc/cache/controlador/ensamblado_controlador/CODIGO/controlador_multi.vhd
@@ -39,7 +39,29 @@ begin
 
 
 
+contro_proc: controlador port map (reloj => reloj, pcero => pcero, arb_pet => arb_peticion, arb_conc => arb_concesion,
+					 trans_bus => trans_bus, pet => pet, s_estado => s_estado, s_control => s_control_P,
+					resp => resp, resp_m => resp_m, pet_m => pet_m);
 
+contro_obser: observador port map (reloj => reloj, pcero => pcero, pet => pet_bus, s_estado => s_estado, trans_bus => trans_bus,
+					observacion => observacion, s_control => s_control_O, resp => t_resp_O);
 
+resp_O <= t_resp_O;
+mxO <= observacion and (not trans_bus);
+
+-- control muxC
+cntl <= observacion and (not trans_bus);
+
+-- muxC
+--s_control <= s_control_P when cntl = '0' else s_control_O;
+s_control.DAT_acc <= s_control_P.DAT_acc;
+s_control.DAT_esc <= s_control_P.DAT_esc;
+--Multiplexados
+s_control.EST_acc <= s_control_P.EST_acc when cntl = '0' else s_control_O.EST_acc;
+s_control.EST_esc <= s_control_P.EST_esc when cntl = '0' else s_control_O.EST_esc;
+s_control.EST_DE  <= s_control_P.EST_DE  when cntl = '0' else s_control_O.EST_DE;
+s_control.ET_acc  <= s_control_P.ET_acc  when cntl = '0' else s_control_O.ET_acc;
+s_control.ET_esc  <= s_control_P.ET_esc  when cntl = '0' else s_control_O.ET_esc;
+s_control.muxE    <= s_control_P.muxE    when cntl = '0' else s_control_O.muxE;
 end;
 
